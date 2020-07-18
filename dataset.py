7e6adc47860a2f391b74c7baf7b627d39812b65f
marzus555 <62560070+marzus555@users.noreply.github.com>
2020-07-18 16:18:20 +0300

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -22,17 +22,17 @@ class MyDataset(Dataset):
 
     def __getitem__(self, index):
         file = self.metadata[index]
-        m = np.load(f"{self.path}mel/{file}.npy")
+        m = np.load(f"{self.path}mel/{file}.npy", allow_pickle=True)
         #x = self.wav_cache[index]
         if 5 > m.shape[-1]:
             print(' [!] Instance is too short! : {}'.format(file))
             self.metadata[index] = self.metadata[index + 1]
             file = self.metadata[index]
-            m = np.load(f"{self.path}mel/{file}.npy")
+            m = np.load(f"{self.path}mel/{file}.npy", allow_pickle=True)
         if self.mode in ['gauss', 'mold']:
             x = self.ap.load_wav(f"{self.path}wavs/{file}.wav")
         elif type(self.mode) is int:
-            x = np.load(f'{self.path}quant/{file}.npy')
+            x = np.load(f'{self.path}quant/{file}.npy', allow_pickle=True)
         else:
             raise RuntimeError("Unknown dataset mode - ", self.mode)
         return m, x, file
