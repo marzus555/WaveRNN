42341a7df0d0f86f1bd9f03e73fc0ff4a32c7592
marzus555 <62560070+marzus555@users.noreply.github.com>
2020-07-30 23:39:22 +0300

    Update audio.py

--- utils/audio.py
+++ utils/audio.py
@@ -1,26 +1,24 @@
-import os
 import librosa
-import pickle
-import copy
-import numpy as np
 import soundfile as sf
-from pprint import pprint
-from scipy import signal, io
+import numpy as np
+import scipy.io
+import scipy.signal
 
 
 class AudioProcessor(object):
     def __init__(self,
-                 bits=None,
                  sample_rate=None,
                  force_sample_rate=False,
                  num_mels=None,
                  min_level_db=None,
                  frame_shift_ms=None,
                  frame_length_ms=None,
+                 hop_length=None,
+                 win_length=None,
                  ref_level_db=None,
                  num_freq=None,
                  power=None,
-                 preemphasis=None,
+                 preemphasis=0.0,
                  signal_norm=None,
                  symmetric_norm=None,
                  max_norm=None,
@@ -30,15 +28,15 @@ class AudioProcessor(object):
                  griffin_lim_iters=None,
                  do_trim_silence=False,
                  trim_db=60,
-                 **kwargs):
+                 sound_norm=False,
+                 **_):
 
         print(" > Setting up Audio Processor...")
 
-        self.bits = bits
         self.sample_rate = sample_rate
         self.force_sample_rate = force_sample_rate
         self.num_mels = num_mels
-        self.min_level_db = min_level_db
+        self.min_level_db = min_level_db or 0
         self.frame_shift_ms = frame_shift_ms
         self.frame_length_ms = frame_length_ms
         self.ref_level_db = ref_level_db
@@ -48,21 +46,27 @@ class AudioProcessor(object):
         self.griffin_lim_iters = griffin_lim_iters
         self.signal_norm = signal_norm
         self.symmetric_norm = symmetric_norm
-        self.mel_fmin = 0 if mel_fmin is None else mel_fmin
+        self.mel_fmin = mel_fmin or 0
         self.mel_fmax = mel_fmax
         self.max_norm = 1.0 if max_norm is None else float(max_norm)
         self.clip_norm = clip_norm
         self.do_trim_silence = do_trim_silence
         self.trim_db = trim_db
-        self.n_fft, self.hop_length, self.win_length = self._stft_parameters()
-        print(" | > Audio Processor attributes.")
+        self.sound_norm = sound_norm
+        if hop_length is None:
+            self.n_fft, self.hop_length, self.win_length = self._stft_parameters()
+        else:
+            self.hop_length = hop_length
+            self.win_length = win_length
+            self.n_fft = (self.num_freq - 1) * 2
+        assert min_level_db != 0.0, " [!] min_level_db is 0"
         members = vars(self)
         for key, value in members.items():
-            print("   | > {}:{}".format(key, value))
+            print(" | > {}:{}".format(key, value))
 
     def save_wav(self, wav, path):
         wav_norm = wav * (32767 / max(0.01, np.max(np.abs(wav))))
-        io.wavfile.write(path, self.sample_rate, wav_norm.astype(np.int16))
+        scipy.io.wavfile.write(path, self.sample_rate, wav_norm.astype(np.int16))
 
     def _linear_to_mel(self, spectrogram):
         _mel_basis = self._build_mel_basis()
@@ -73,23 +77,23 @@ class AudioProcessor(object):
         return np.maximum(1e-10, np.dot(inv_mel_basis, mel_spec))
 
     def _build_mel_basis(self, ):
-        n_fft = (self.num_freq - 1) * 2
         if self.mel_fmax is not None:
             assert self.mel_fmax <= self.sample_rate // 2
         return librosa.filters.mel(
             self.sample_rate,
-            n_fft,
+            self.n_fft,
             n_mels=self.num_mels,
             fmin=self.mel_fmin,
             fmax=self.mel_fmax)
 
     def _normalize(self, S):
         """Put values in [0, self.max_norm] or [-self.max_norm, self.max_norm]"""
+        #pylint: disable=no-else-return
         if self.signal_norm:
             S_norm = ((S - self.min_level_db) / - self.min_level_db)
             if self.symmetric_norm:
                 S_norm = ((2 * self.max_norm) * S_norm) - self.max_norm
-                if self.clip_norm :
+                if self.clip_norm:
                     S_norm = np.clip(S_norm, -self.max_norm, self.max_norm)
                 return S_norm
             else:
@@ -102,18 +106,19 @@ class AudioProcessor(object):
 
     def _denormalize(self, S):
         """denormalize values"""
+        #pylint: disable=no-else-return
         S_denorm = S
         if self.signal_norm:
             if self.symmetric_norm:
                 if self.clip_norm:
-                    S_denorm = np.clip(S_denorm, -self.max_norm, self.max_norm) 
+                    S_denorm = np.clip(S_denorm, -self.max_norm, self.max_norm)
                 S_denorm = ((S_denorm + self.max_norm) * -self.min_level_db / (2 * self.max_norm)) + self.min_level_db
                 return S_denorm
             else:
                 if self.clip_norm:
                     S_denorm = np.clip(S_denorm, 0, self.max_norm)
                 S_denorm = (S_denorm * -self.min_level_db /
-                    self.max_norm) + self.min_level_db
+                            self.max_norm) + self.min_level_db
                 return S_denorm
         else:
             return S
@@ -121,28 +126,29 @@ class AudioProcessor(object):
     def _stft_parameters(self, ):
         """Compute necessary stft parameters with given time values"""
         n_fft = (self.num_freq - 1) * 2
+        factor = self.frame_length_ms / self.frame_shift_ms
+        assert (factor).is_integer(), " [!] frame_shift_ms should divide frame_length_ms"
         hop_length = int(self.frame_shift_ms / 1000.0 * self.sample_rate)
-        win_length = int(self.frame_length_ms / 1000.0 * self.sample_rate)
-        print(" | > fft size: {}, hop length: {}, win length: {}".format(
-            n_fft, hop_length, win_length))
+        win_length = int(hop_length * factor)
         return n_fft, hop_length, win_length
 
     def _amp_to_db(self, x):
         min_level = np.exp(self.min_level_db / 20 * np.log(10))
         return 20 * np.log10(np.maximum(min_level, x))
 
-    def _db_to_amp(self, x):
+    @staticmethod
+    def _db_to_amp(x):
         return np.power(10.0, x * 0.05)
 
     def apply_preemphasis(self, x):
         if self.preemphasis == 0:
-            raise RuntimeError(" !! Preemphasis is applied with factor 0.0. ")
-        return signal.lfilter([1, -self.preemphasis], [1], x)
+            raise RuntimeError(" [!] Preemphasis is set 0.0.")
+        return scipy.signal.lfilter([1, -self.preemphasis], [1], x)
 
     def apply_inv_preemphasis(self, x):
         if self.preemphasis == 0:
-            raise RuntimeError(" !! Preemphasis is applied with factor 0.0. ")
-        return signal.lfilter([1], [1, -self.preemphasis], x)
+            raise RuntimeError(" [!] Preemphasis is set 0.0.")
+        return scipy.signal.lfilter([1], [1, -self.preemphasis], x)
 
     def spectrogram(self, y):
         if self.preemphasis != 0:
@@ -167,8 +173,7 @@ class AudioProcessor(object):
         # Reconstruct phase
         if self.preemphasis != 0:
             return self.apply_inv_preemphasis(self._griffin_lim(S**self.power))
-        else:
-            return self._griffin_lim(S**self.power)
+        return self._griffin_lim(S**self.power)
 
     def inv_mel_spectrogram(self, mel_spectrogram):
         '''Converts mel spectrogram to waveform using librosa'''
@@ -177,14 +182,21 @@ class AudioProcessor(object):
         S = self._mel_to_linear(S)  # Convert back to linear
         if self.preemphasis != 0:
             return self.apply_inv_preemphasis(self._griffin_lim(S**self.power))
-        else:
-            return self._griffin_lim(S**self.power)
+        return self._griffin_lim(S**self.power)
+
+    def out_linear_to_mel(self, linear_spec):
+        S = self._denormalize(linear_spec)
+        S = self._db_to_amp(S + self.ref_level_db)
+        S = self._linear_to_mel(np.abs(S))
+        S = self._amp_to_db(S) - self.ref_level_db
+        mel = self._normalize(S)
+        return mel
 
     def _griffin_lim(self, S):
         angles = np.exp(2j * np.pi * np.random.rand(*S.shape))
         S_complex = np.abs(S).astype(np.complex)
         y = self._istft(S_complex * angles)
-        for i in range(self.griffin_lim_iters):
+        for _ in range(self.griffin_lim_iters):
             angles = np.exp(1j * np.angle(self._stft(y)))
             y = self._istft(S_complex * angles)
         return y
@@ -195,6 +207,7 @@ class AudioProcessor(object):
             n_fft=self.n_fft,
             hop_length=self.hop_length,
             win_length=self.win_length,
+            pad_mode='constant'
         )
 
     def _istft(self, y):
@@ -211,7 +224,7 @@ class AudioProcessor(object):
         return len(wav)
 
     def trim_silence(self, wav):
-        """ Trim silent parts with a threshold and 0.1 sec margin """
+        """ Trim silent parts with a threshold and 0.01 sec margin """
         margin = int(self.sample_rate * 0.01)
         wav = wav[margin:-margin]
         return librosa.effects.trim(
@@ -224,7 +237,7 @@ class AudioProcessor(object):
         signal = np.sign(wav) * np.log(1 + mu * np.abs(wav)) / np.log(1. + mu)
         # Quantize signal to the specified number of levels.
         signal = (signal + 1) / 2 * mu + 0.5
-        return np.floor(signal)
+        return np.floor(signal,)
 
     @staticmethod
     def mulaw_decode(wav, qc):
@@ -241,15 +254,23 @@ class AudioProcessor(object):
         else:
             x, sr = librosa.load(filename, sr=sr)
         if self.do_trim_silence:
-            x = self.trim_silence(x)
+            try:
+                x = self.trim_silence(x)
+            except ValueError:
+                print(f' [!] File cannot be trimmed for silence - {filename}')
         assert self.sample_rate == sr, "%s vs %s"%(self.sample_rate, sr)
+        if self.sound_norm:
+            x = x / abs(x).max() * 0.9
         return x
 
-    def encode_16bits(self, x):
+    @staticmethod
+    def encode_16bits(x):
         return np.clip(x * 2**15, -2**15, 2**15 - 1).astype(np.int16)
 
-    def quantize(self, x):
-        return (x + 1.) * (2**self.bits - 1) / 2
+    @staticmethod
+    def quantize(x, bits):
+        return (x + 1.) * (2**bits - 1) / 2
 
-    def dequantize(self, x):
-        return 2 * x / (2**self.bits - 1) - 1
+    @staticmethod
+    def dequantize(x, bits):
+        return 2 * x / (2**bits - 1) - 1
